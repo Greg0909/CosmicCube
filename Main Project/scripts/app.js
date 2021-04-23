6305fda10ecf99456b985e8b6656fb09176636d4
Greg Espinosa Carrillo <grega1196764@gmail.com>
2021-04-22 19:30:34 -0500

    Se removio el proyecto de node-selenium para evitar posibles problemas. Ademas se incluyo un boton en la web app que borra el texto del analisis, para poder volver a tomar fotos sin tener que refrescar la pagina

--- Main Project/scripts/app.js
+++ Main Project/scripts/app.js
@@ -2,3 +2,7 @@
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+document.getElementById("clearText").onclick = (e) => {
+    document.getElementById("ticketTextDisplay").innerText = "";
+}
